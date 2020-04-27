bbdda07d13cac309734eb9e3a0963240a648ceea
Andres <andresjpulido@gmail.com>
2020-04-27 23:25:17 +1200

    add new button

---
+++ src/components/Message.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (<div className="message">
+            God Job, YOU WIN!
+        </div>)
+    }
+
+}
+
+export default Message;
